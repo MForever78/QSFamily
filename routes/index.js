1976284cc2a33f3cdbfe4c9186a6391e2dd4d662
MForever78 <shunjian1128@gmail.com>
2015-05-09 22:33:14 +0800

    Add error handling route

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,16 @@ module.exports = function(app) {
     if (key === 'index') return;
     app.use('/' + key, routes[key]);
   });
+
+  // error handling
+  app.use(function(req, res, next) {
+    // nothing found
+    res.json({ code: Message.notFound });
+  });
+
+  app.use(function(err, req, res, next) {
+    // error
+    res.json({ code: Message.internalServerError });
+  });
 };
 
