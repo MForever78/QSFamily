ae08e8e76d5512f580a973fd98c87835354f18b6
MForever78 <shunjian1128@gmail.com>
2015-05-09 16:19:24 +0800

    add list user route

---
+++ models/user.js
@@ -0,0 +1,22 @@
+/**
+ * Created by MForever78 on 15/5/9.
+ */
+
+var express = require('express');
+var app = module.exports = express();
+
+app.get('/:role/:userid', function(req, res, next) {
+  User(req.params.role, req.params.userid)
+    .then(function(user) {
+      if (!user) {
+        res.json({
+          code: Message.notFound
+        });
+      }
+      res.json(user);
+    })
+    .catch(function(err) {
+      err.message = "Can't list user file";
+      next(err);
+    });
+});
