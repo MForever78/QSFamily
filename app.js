e653482b47e48817a161ab68880b06aa8ce163bf
MForever78 <shunjian1128@gmail.com>
2015-09-05 15:08:48 +0800

    Add morgan logger

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var config = require('config');
+var morgan = require('morgan');
 var port = process.env.PORT || 3000;
 
 /* initialize body parser */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/* logger */
+app.use(morgan('dev'));
+
 /* jwt control */
 var Jwt = require('express-jwt');
 
 var jwtMiddleWare = Jwt({
   secret: config.get('jwtSecret'),
-  credentialsRequired: false,
-  getToken: function(req) {
-    if (!req.body && !req.query.token) return null;
-    return req.body.token || req.query.token;
-  }
+  credentialsRequired: false
 });
 
 app.use(jwtMiddleWare);
@@ -44,3 +44,4 @@ require('./models');
 app.listen(port, function() {
   console.log("Server listening on port: " + port + "...");
 });
+
