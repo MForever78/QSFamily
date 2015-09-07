0b7f8bd1d2459f14da215953ec9c57b4b1acd9be
MForever78 <shunjian1128@gmail.com>
2015-09-07 16:08:31 +0800

    Add course CRUD function

--- models/course.js
+++ models/course.js
@@ -4,6 +4,26 @@ exports.getCourseList = function() {
   return Knex('course');
 };
 
+exports.getCourseById = function(courseId) {
+  return Knex('course')
+    .where("id", courseId);
+};
+
+exports.updateCourse = function(courseId, data) {
+  return Knex('course')
+    .where('id', courseId)
+    .update({
+      course_name: data.course_name,
+      description: data.description
+    });
+};
+
+exports.deleteCourse = function(id) {
+  return Knex('course')
+    .where('id', id)
+    .del();
+};
+
 exports.createCourse = function(data) {
   return Knex('course')
     .insert(data);
--- routes/course.js
+++ routes/course.js
@@ -2,6 +2,59 @@ var express = require('express');
 var app = module.exports = express();
 var debug = require('debug')('QSFamily:courseRoute');
 
+app.get('/:courseId', function(req, res, next) {
+  debug('Getting course', req.params.courseId);
+  Course.getCourseById(req.params.courseId)
+    .then(function(course) {
+      debug('Found course', req.params.courseId);
+      res.json({
+        code: Message.ok,
+        course: course[0]
+      });
+    });
+});
+
+app.post('/', function(req, res, next) {
+  debug('Creating new course');
+  Course.createCourse(req.body)
+    .then(function(id) {
+      debug('Created course', id);
+      Course.getCourseById(id)
+        .then(function(course) {
+          res.json({
+            code: Message.ok,
+            course: course[0]
+          });
+        });
+    });
+});
+
+app.put('/:courseId', function(req, res, next) {
+  debug('Updating course', req.params.courseId);
+  Course.updateCourse(req.params.courseId, req.body)
+    .then(function() {
+      debug('Update', req.params.coursId, "succeed");
+      Course.getCourseById(req.params.courseId)
+        .then(function(course) {
+          res.json({
+            code: Message.ok,
+            course: course[0]
+          });
+        });
+    });
+});
+
+app.delete('/:courseId', function(req, res, next) {
+  debug('Deleting course', req.params.courseId);
+  Course.deleteCourse(req.params.courseId)
+    .then(function() {
+      debug('deleted course', req.params.courseId);
+      res.json({
+        code: Message.ok
+      });
+    });
+});
+
 app.get('/', function(req, res, next) {
   debug('Getting course list');
   Course.getCourseList()
