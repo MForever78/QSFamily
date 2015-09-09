991c5d7150428328a68d0d2e02c13b1e11c09ab1
MForever78 <shunjian1128@gmail.com>
2015-09-09 23:23:17 +0800

    change database schema: delete course module table

--- models/course.js
+++ models/course.js
@@ -28,31 +28,3 @@ exports.createCourse = function(data) {
   return Knex('course')
     .insert(data);
 };
-
-exports.getCourseModules = function(courseId) {
-  return Knex('course_module')
-    .where('course_id', courseId);
-};
-
-exports.getCourseModulesById = function(id) {
-  return Knex('course_module')
-    .where('id', id);
-};
-
-exports.createCourseModule = function(data) {
-  return Knex('course_module')
-    .insert(data);
-};
-
-exports.updateCourseModule = function(id, data) {
-  return Knex('course_module')
-    .where('id', id)
-    .update(data);
-};
-
-exports.deleteCourseModule = function(id) {
-  return Knex('course_module')
-    .where('id', id)
-    .del();
-};
-
