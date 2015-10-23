6b8a168db534cc2c1fa61844de729a827dc58ce1
MForever78 <shunjian1128@gmail.com>
2015-10-23 16:31:36 +0800

    build

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * Created by MForever78 on 15/10/21.
+ */
+
+$(function() {
+  function bindEvents() {
+    $('.course-tab').on('click', function(e) {
+      e.preventDefault();
+      var tab = $(e.currentTarget);
+      tab.addClass("active");
+      tab.siblings().removeClass("active");
+      // active tab content
+      var content = $(e.target.dataset.target);
+      // remove in first to let animation work
+      content.siblings().removeClass("in");
+      setTimeout(function () {
+        content.siblings().removeClass("active");
+        content.addClass("active in");
+      }, 200);
+    }).first().find('a').click();
+
+    $('a[data-action=edit]').on('click', function(e) {
+      window.href = '/edit/course/' + e.target.dataset.id;
+    });
+
+    $('a[data-action=delete]').on('click', function() {
+      // TODO: delete the course
+    });
+
+    $('a[data-action=new]').on('click', function() {
+      window.href = '/write/course';
+    });
+  }
+  // binding
+  bindEvents();
+});
