a4c5c850d04d8b8dd2f6d7833e0e501f56b501b1
MForever78 <shunjian1128@gmail.com>
2015-10-24 00:07:37 +0800

    Add news model

---
+++ models/news.js
@@ -0,0 +1,35 @@
+/**
+ * Created by MForever78 on 15/10/23.
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var newsSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+
+  author: {
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+
+  content: String,
+
+  comments: [Schema.Types.ObjectId],
+  create_at: {
+    type: Date,
+    "default": Date.now(),
+    required: true
+  },
+  update_at: Date
+});
+
+newsSchema.post('save', function() {
+  this.update_at = Date.now();
+});
+
+var News = mongoose.model('News', newsSchema);
+module.exports = News;
