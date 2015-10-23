3f5bce5ce00192c862b1769ddc710c35fc1fdb34
MForever78 <shunjian1128@gmail.com>
2015-10-23 22:37:06 +0800

    Add teacher model

--- models/teacher.js
+++ models/teacher.js
@@ -1,3 +1,19 @@
 /**
  * Created by MForever78 on 15/10/23.
  */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var User = require('./user');
+
+var teacherSchema = new Schema(
+  {
+    email: String
+  },
+  {
+    discriminatorKey: 'role'
+  }
+);
+
+var Teacher = User.discriminator('teacher', teacherSchema);
+module.exports = Teacher;
