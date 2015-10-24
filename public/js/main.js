ef64f42a2c1a614df30d2ed98cb22e71102ee8c2
MForever78 <shunjian1128@gmail.com>
2015-10-24 22:42:57 +0800

    build

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,9 @@
  */
 
 $(function() {
+  /*
+    Write
+   */
   var textarea = $('#write-content');
   if (textarea.length !== 0) {
     var editor = new Simditor({
@@ -10,37 +13,36 @@ $(function() {
     });
   }
 
-  function bindEvents() {
-    /*
-      Course
-     */
-    $('.course-tab').on('click', function(e) {
-      e.preventDefault();
-      var tab = $(e.currentTarget);
-      tab.addClass("active");
-      tab.siblings().removeClass("active");
-      // active tab content
-      var content = $(e.target.dataset.target);
-      // remove in first to let animation work
-      content.siblings().removeClass("in");
-      setTimeout(function () {
-        content.siblings().removeClass("active");
-        content.addClass("active in");
-      }, 200);
-    }).first().find('a').click();
+  $('#write-form').on('submit', function(e) {
+  });
 
-    $('a[data-action=edit]').on('click', function(e) {
-      window.href = '/edit/course/' + e.target.dataset.id;
-    });
+  /*
+    Course
+   */
+  $('.course-tab').on('click', function(e) {
+    e.preventDefault();
+    var tab = $(e.currentTarget);
+    tab.addClass("active");
+    tab.siblings().removeClass("active");
+    // active tab content
+    var content = $(e.target.dataset.target);
+    // remove in first to let animation work
+    content.siblings().removeClass("in");
+    setTimeout(function () {
+      content.siblings().removeClass("active");
+      content.addClass("active in");
+    }, 200);
+  }).first().find('a').click();
 
-    $('a[data-action=delete]').on('click', function() {
-      // TODO: delete the course
-    });
+  $('a[data-action=edit]').on('click', function(e) {
+    window.href = '/edit/course/' + e.target.dataset.id;
+  });
 
-    $('a[data-action=new]').on('click', function() {
-      window.href = '/write/course';
-    });
-  }
-  // binding
-  bindEvents();
+  $('a[data-action=delete]').on('click', function() {
+    // TODO: delete the course
+  });
+
+  $('a[data-action=new]').on('click', function() {
+    window.href = '/write/course';
+  });
 });
