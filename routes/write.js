2d3875bcf20a5393860ab914b3401297448dbd38
MForever78 <shunjian1128@gmail.com>
2015-10-24 22:43:36 +0800

    Add post function

--- routes/write.js
+++ routes/write.js
@@ -4,14 +4,26 @@
 
 var app = require('express')();
 var debug = require('debug')('QSFamily:route:write');
+var auth = require('../middleware/auth');
 
-app.get('/', function(req, res, next) {
-  if (!req.session.user || !req.session.user.role === 'Teacher') return res.redirect('/');
+app.get('/', auth('Teacher'), function(req, res, next) {
   res.render('write', {
     session: req.session,
     action: '/write',
     method: 'post'
   });
+});
+
+app.post('/', auth('Teacher'), function(req, res, next) {
+  var news = {
+    title: req.body.title,
+    author: req.session.user._id,
+    content: req.body.content
+  };
+  News.create(news)
+    .then(function() {
+      return res.json({code: 0});
+    });
 });
 
 module.exports = app;
