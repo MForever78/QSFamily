2fe16b090980cc85a370bdf461e7b4f4be2d7cc9
MForever78 <shunjian1128@gmail.com>
2015-10-25 18:21:27 +0800

    Populate author before render

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,17 @@ var routes = require('node-require-directory')(__dirname);
 module.exports = function(app) {
   app.use('/$', function(req, res, next) {
     News.find({})
+      .populate('author')
       .then(function(newsList) {
+        // Add following property:
+        //    news.date with format: "XXXX 年 YY 月 ZZ 日"
+        //    news.url: "/news/<ObjectId>"
+        newsList = newsList.map(function(news) {
+          var date = news.createAt;
+          news.date = date.getFullYear() + ' 年 ' + (date.getMonth() + 1) + ' 月 ' + date.getDate() + ' 日';
+          news.url = "/news/" + news._id;
+          return news;
+        });
         res.render('index', {
           session: req.session,
           newsList: newsList
