692ab53f2c5e417631544b25286fe9a3959a7bec
MForever78 <shunjian1128@gmail.com>
2015-10-27 23:57:15 +0800

    accept an array of roles

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,9 +2,14 @@
  * Created by MForever78 on 15/10/24.
  */
 
-module.exports = function(role) {
+module.exports = function(roles) {
+  if (roles.constructor !== Array) {
+    var role = roles;
+    roles = [];
+    roles.push(role);
+  }
   return function(req, res, next) {
-    if (!(req.session.user && req.session.user.role === role)) {
+    if (!(req.session.user && roles.indexOf(req.session.user.role) !== -1)) {
       res.session = null;
       return res.redirect('/');
     }
