257f14bf3c0e05a05934d1ca434077e887136b2d
MForever78 <shunjian1128@gmail.com>
2015-10-28 01:15:54 +0800

    Add post course route

--- routes/course.js
+++ routes/course.js
@@ -1,11 +1,30 @@
 var app = require('express')();
+var auth = require('../middleware/auth');
 
 app.get('/', function(req, res, next) {
-  var editable = req.session.user && req.session.user.role === 'Teacher';
-  var courseList = [];
-  res.render('course-list', {
+  return Course.find({})
+    .then(function(courseList) {
+      return res.render('course-list', {
+        session: req.session,
+        courseList: courseList
+      });
+    });
+});
+
+app.get('/new', function(req, res, next) {
+  res.render('write', {
     session: req.session,
-    courseList: courseList
+    action: '/course',
+    method: 'post'
+  });
+});
+
+app.post('/', auth("Teacher"), function(req, res, next) {
+  return Course.create({
+    name: req.body.title,
+    description: req.body.content
+  }).then(function() {
+    return res.json({code: 0});
   });
 });
 
