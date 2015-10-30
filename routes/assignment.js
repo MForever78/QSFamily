6a36c0792bdb3f80f0df1608ba09bd6cb6df841b
MForever78 <shunjian1128@gmail.com>
2015-10-30 19:46:27 +0800

    Add assignment to course and corresponding students

--- routes/assignment.js
+++ routes/assignment.js
@@ -6,15 +6,36 @@ var app = require('express')();
 var debug = require('debug')('QSFamily:route:assignment');
 
 app.post('/', function(req, res, next) {
+  var outer = {};
   return Assignment.create({
     title: req.body.title,
-    course: req.body.courseId,
+    course: req.body.course,
     description: req.body.description,
     dueDate: new Date(req.body.dueDate),
     deadline: new Date(req.body.deadline)
   }).then(function(assignment) {
-    // TODO: add new assignment to course and corresponding student
-    return res.json({code: 0});
+    // add the new assignment to course
+    outer.assignment = assignment;
+    return Course.findByIdAndUpdate(req.body.course, {
+      $push: { assignments: assignment._id }
+    });
+  }).then(function() {
+    debug("Add assignment to course model SUCCEED");
+    // add the new assignment to corresponding students
+    return Student.update({
+      courseTaking: req.body.course
+    }, {
+      $push: {
+        assignments: {
+          reference: outer.assignment._id
+        }
+      }
+    }, {
+      multi: true
+    })
+  }).then(function() {
+    debug("Add assignment to student model SUCCEED");
+    return res.json({ code: 0 });
   });
 });
 
