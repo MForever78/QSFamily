90106ffa89125c999c95db923c25eacbd3ea0d9a
MForever78 <shunjian1128@gmail.com>
2015-10-31 15:27:01 +0800

    Add register model

---
+++ models/register.js
@@ -0,0 +1,16 @@
+/**
+ * Created by MForever78 on 15/10/31.
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var registerSchema = new Schema({
+  studentId: String,
+  name: String,
+  department: String,
+  gender: String
+});
+
+var Register = mongoose.model('Register', registerSchema);
+module.exports = Register;
