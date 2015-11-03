4cc87a9dd52d37903be0b1c057c6c7171a174b0d
MForever78 <shunjian1128@gmail.com>
2015-11-03 14:55:08 +0800

    Add upload assignment controller

--- routes/assignment.js
+++ routes/assignment.js
@@ -4,6 +4,21 @@
 
 var app = require('express')();
 var debug = require('debug')('QSFamily:route:assignment');
+var auth = require('../middleware/auth');
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: __dirname + '/../../upload/assignments',
+  filename: function(req, file, callback) {
+    Student.findById(req.session.user._id)
+      .then(function(student) {
+        var ext = file.originalname.split('.').pop();
+        var filename = [student.studentId, student.name, req.body.title].join('_');
+        filename += '.' + ext;
+        callback(null, filename);
+      })
+  }
+});
+var upload = multer({ storage: storage });
 
 app.post('/', function(req, res, next) {
   var outer = {};
@@ -45,6 +60,23 @@ app.delete('/', function(req, res, next) {
       return res.json({ code: 0 });
     }).catch(function(err) {
       next(err);
+    });
+});
+
+app.post('/upload', auth("Student"), upload.single('file'), function(req, res, next) {
+  debug("Uploaded assignment:");
+  debug(req.body);
+  return Student.findById(req.session.user._id)
+    .then(function(student) {
+      student.assignments = student.assignments.map(function(assignment) {
+        if (assignment._id == req.body.assignment) {
+          assignment.complete = true;
+        }
+        return assignment;
+      });
+      return student.save();
+    }).then(function() {
+      return res.json({code: 0});
     });
 });
 
