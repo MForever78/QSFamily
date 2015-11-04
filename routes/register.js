b1e39ed4422ef63b6b30fa7afca47099e07c8a0f
MForever78 <shunjian1128@gmail.com>
2015-11-04 11:08:38 +0800

    Complete register controller

--- routes/register.js
+++ routes/register.js
@@ -12,6 +12,10 @@ var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
 var XLSX = require('xlsx');
 
+app.get('/', function(req, res, next) {
+  res.render('register');
+});
+
 function cookPassword(key, salt) {
   var hash = crypto.createHash('sha512');
   return hash.update(key.slice(0, saltPos))
@@ -21,6 +25,65 @@ function cookPassword(key, salt) {
 }
 
 app.post('/', function(req, res, next) {
+  var salt = crypto.randomBytes(64).toString('base64');
+  debug("New student:");
+  debug("Username:", req.body.username);
+  var outer = {};
+  // Find student form register
+  return Register.findOne({
+    $and: [
+      {studentId: req.body.studentId},
+      {name: req.body.name}
+    ]
+  }).then(function(student) {
+    if (!student) {
+      return res.render('register', { error: true });
+    }
+    var user = {
+      username: req.body.username,
+      salt: salt,
+      password: cookPassword(req.body.password, salt),
+      studentId: student.studentId,
+      name: student.name,
+      gender: student.gender,
+      department: student.department,
+      courseTaking: student.course
+    };
+    outer.student = student;
+    // create user and query course
+    var addStudent =  Student.create(user);
+    var queryCourse = Course.findById(student.course);
+    return Promise.all([addStudent, queryCourse]);
+  }).then(function(result) {
+    var student = result[0];
+    var course = result[1];
+    var cookedAssignments = course.assignments.map(function(assignment) {
+      var cookedAssignment = {};
+      cookedAssignment.reference = assignment;
+      return cookedAssignment;
+    });
+    // add assignments to student
+    return student.update({
+      $push: {
+        assignments: { $each: cookedAssignments }
+      }
+    });
+  }).then(function() {
+    // remove student from register
+    return outer.student.remove();
+  }).then(function() {
+    return res.render('login', {
+      message: {
+        type: 'success',
+        text: '注册成功, 请登陆'
+      }
+    });
+  }).catch(function(err) {
+    next(err);
+  });
+});
+
+app.post('/grant', function(req, res, next) {
   var salt = crypto.randomBytes(64).toString('base64');
   debug("New user:");
   debug("Username:", req.body.username);
@@ -46,43 +109,6 @@ app.post('/', function(req, res, next) {
         });
     default: return res.json({code: -1, message: "Invalid user role"});
   }
-});
-
-app.post('/', function(req, res, next) {
-  var salt = crypto.randomBytes(64).toString('base64');
-  debug("New student:");
-  debug("Username:", req.body.username);
-  var outer = {};
-  return Register.findOne({
-    $and: [
-      {studentId: req.body.studentId},
-      {name: req.body.name}
-    ]
-  }).then(function(student) {
-    if (!student) {
-      return res.render('register', { error: true });
-    }
-    var user = {
-      username: req.body.username,
-      salt: salt,
-      password: cookPassword(req.body.password, salt),
-      studentId: student.studentId,
-      name: student.name,
-      gender: student.gender,
-      department: student.department
-    };
-    outer.student = student;
-    return Student.create(user);
-  }).then(function() {
-    return outer.student.remove();
-  }).then(function() {
-    return res.render('login', {
-      message: {
-        type: 'success',
-        message: '注册成功, 请登陆'
-      }
-    });
-  });
 });
 
 app.post('/sheet', auth("Teacher"), upload.single('file'), function(req, res, next) {
